2c8f36aeaa7c67de46d7cf3a2ddf18fdb5f704a8
vichet vatt <vichetvattedu@gmail.com>
2023-10-27 19:30:53 +0700

    exporting requiring module

---
+++ module.js
@@ -0,0 +1,16 @@
+// class FuncModule {
+//     addFunc(a, b) {
+//         return a + b;
+//     }
+// }
+// //Exporting FuncModule
+// module.exports = FuncModule;
+//##################################//
+
+// module.exports = class {
+//     addFunc(a, b) {
+//         return a + b;
+//     }
+// }
+
+exports.add = (a, b) => a + b;
---
+++ test_module.js
@@ -0,0 +1,11 @@
+// importing module from class FuncModule from file (module.js)
+// const FuncModule = require('./module');
+
+// const cal = new FuncModule();
+
+// console.log(cal.addFunc(1, 2));
+
+//####################
+
+const funcAdd = require('./module');
+console.log(funcAdd.add(2, 5));
